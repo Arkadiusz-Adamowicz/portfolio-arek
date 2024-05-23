fa5a8f9ad45ad7451367ab0bf19103f29f661bee
Arek <arek.adamowicz81@gmail.com>
2024-05-23 12:58:05 +0200

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import Hero from '@/components/Hero';
-import { FloatingNav } from '@/components/ui/FloatingNav';
-import { FaHome } from 'react-icons/fa';
-import Grid from '@/components/Grid';
+import React from "react";
+import Hero from "@/components/Hero";
+import { FloatingNav } from "@/components/ui/FloatingNav";
+import { FaHome } from "react-icons/fa";
+import Grid from "@/components/Grid";
 
 const Home = () => {
   return (
-    <main className='relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5'>
-      <div className='max-w-7xl w-full'>
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
         <FloatingNav
-          navItems={[{ name: 'Home', link: '/', icon: <FaHome /> }]}
+          navItems={[{ name: "Home", link: "/", icon: <FaHome /> }]}
         />
         <Hero />
         <Grid />
