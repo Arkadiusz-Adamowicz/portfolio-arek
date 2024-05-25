4a26181b6f4a3e25d941c2691c8d21bcc8b65c8f
Arkadiusz Adamowicz <arek.adamowicz81@gmail.com>
2024-05-25 09:22:59 +0200

    update

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,8 @@ import RecentProjects from '@/components/RecentProjects';
 import { navItems } from '@/data';
 import Clients from '@/components/Clients';
 import Experience from '@/components/Experience';
+import Approach from '@/components/Approach';
+import Footer from '@/components/Footer';
 
 const Home = () => {
   return (
@@ -18,6 +20,8 @@ const Home = () => {
         <RecentProjects />
         <Clients />
         <Experience />
+        <Approach />
+        <Footer />
       </div>
     </main>
   );
